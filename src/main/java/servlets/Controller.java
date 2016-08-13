caa3671d0a5c910043f4492708f918ce0a83e4e3
User <User>
2016-08-13 16:04:54 +0300

    first working commit

---
+++ src/main/java/servlets/Controller.java
@@ -0,0 +1,35 @@
+package servlets;
+
+import beans.UserBean;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Iurii_Kryshtop on 8/11/2016.
+ */
+
+@WebServlet("/servlet")
+public class Controller extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        String surname = req.getParameter("surname");
+
+        UserBean userBean = new UserBean();
+        userBean.setName(name);
+        userBean.setSurname(surname);
+//        req.setAttribute("name", name);
+//        req.setAttribute("surname", surname);
+        req.getSession().setAttribute("user", userBean);
+        req.getRequestDispatcher("page.jsp").forward(req, resp);
+    }
+}
